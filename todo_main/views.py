bd5890032e8e09f7e222c1a026ad03961015a1c3
Ankit Bisht <Ankitbisht.0107@gmail.com>
2024-07-06 14:42:57 +0530

    modified addtask functionality

--- todo_main/views.py
+++ todo_main/views.py
@@ -3,7 +3,11 @@ from todo.models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_completed = False)
+    completedtasks = Task.objects.filter(is_completed = True)
     data = {
         'tasks': tasks,
+        'completedtasks' : completedtasks,
     }
+    completedtasks = Task.objects.filter(is_completed = True)
+
     return render(request, 'index.html',data)
